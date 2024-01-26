6fe8a0f8ff88d4ad453c10697d33115eb730194d
Makito <dmakito662@gmail.com>
2024-01-26 23:38:40 +0200

    updated files

---
+++ server/src/db/index.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import { MONGO_URI } from "../utils/virable";
+
+mongoose.connect(MONGO_URI).then(() => {
+console.log("db is connected");
+
+}).catch((err)=>{
+    console.log("db failed",err)
+})
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import  "dotenv/config";
+import "./db";
 
 const app = express();
 
